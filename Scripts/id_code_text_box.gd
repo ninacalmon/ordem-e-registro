741b085fa50b77a4b6debf4ba495387a322c5dd7
Nina <calmon.nina01@gmail.com>
2026-02-24 15:49:30 -0300

    Adjusted Id line edits font, size, colors, and system to automatically insert '/' and '-' in birth date and code.

--- Scripts/id_code_text_box.gd
+++ Scripts/id_code_text_box.gd
@@ -1,33 +1,79 @@
 extends LineEdit
 
 enum InputCodeTypes {
-	ID_CODE,
-	ID_BIRTH_DATE
+	ID_NAME,
+	ID_BIRTH_DATE,
+	ID_CODE
 }
 
 @export var focusable_module: FocusableModule
 @export var value_type: InputCodeTypes
 
+var already_submitted: bool = false
+
 func _ready():
 	self.text_submitted.connect(_on_line_input_submitted)
+	self.text_changed.connect(_on_line_text_changed)
 
 func _process(_delta: float) -> void:
 	if !self.focusable_module.is_focused:
-		self.hide()
+		self.editable = false
+		self.release_focus()
 		return
 	
-	self.show()
+	if !self.already_submitted:
+		self.editable = true
+
+func _on_line_text_changed(new_text):
+	match value_type:
+		InputCodeTypes.ID_BIRTH_DATE:
+			format_birth_date(new_text)
+		
+		InputCodeTypes.ID_CODE:
+			format_id_code(new_text)
+		
+		_:
+			return
 
 func _on_line_input_submitted(new_text: String):
 	var expected: String = ""
 	
 	match value_type:
-		InputCodeTypes.ID_CODE:
-			expected = CustomerInfo.consolidated_id_code
+		InputCodeTypes.ID_NAME:
+			expected = CustomerInfo.child_complete_name
 		InputCodeTypes.ID_BIRTH_DATE:
 			expected = CustomerInfo.child_birth_date
+		InputCodeTypes.ID_CODE:
+			expected = CustomerInfo.consolidated_id_code
 	
 	if new_text.to_upper() == expected.to_upper():
 		print("PARABÉNS, ACERTOU O ID!")
-		editable = false
-		release_focus()
+
+	self.editable = false
+	self.already_submitted = true
+	release_focus()
+
+func format_birth_date(new_text: String):
+	text_changed.disconnect(_on_line_text_changed)
+	
+	if new_text.length() == 2 or new_text.length() == 5:
+		if !new_text.ends_with("/"):
+			new_text += "/"
+	
+	text = new_text
+	set_caret_column(text.length())
+	
+	text_changed.connect(_on_line_text_changed)
+
+func format_id_code(new_text: String):
+	text_changed.disconnect(_on_line_text_changed)
+	
+	if new_text.length() == 2 or new_text.length() == 6:
+		if !new_text.ends_with("-"):
+			new_text += "-"
+	
+	text = new_text
+	set_caret_column(text.length())
+	
+	text_changed.connect(_on_line_text_changed)
+	
