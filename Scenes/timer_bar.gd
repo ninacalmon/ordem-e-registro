18036e392bae86dd2814aee00a6ef277165b693b
Nina <calmon.nina01@gmail.com>
2026-02-27 00:13:43 -0300

    Add music to game. Fix bug where stamping on the last second would make the game crash

--- Scenes/timer_bar.gd
+++ Scenes/timer_bar.gd
@@ -3,5 +3,5 @@ extends TextureProgressBar
 @onready var new_docs_timer: Timer = %NewDocsTimer
 
 func _process(_delta: float) -> void:
-	self.max_value = new_docs_timer.wait_time
+	self.max_value = new_docs_timer.wait_time - 1
 	value = new_docs_timer.time_left
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -87,6 +87,10 @@ func instanciate_documents():
 	EventBus.docs_arrived_at_final_position.emit()
 
 func _on_document_stamped():
+	# Avoid stamping when timer is close to timeout
+	if new_docs_timer.time_left <= 1:
+		return
+
 	print(score_bar.value)
 	
 	if score_bar.value < 2.3:
